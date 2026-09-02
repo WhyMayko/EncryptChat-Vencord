83c67833b876e8b821c1ed2167e44e47f603e0ea
Mayko <maykonascimentodasilvasilva@gmail.com>
2026-09-02 19:01:19 -0300

    fix: pure white text in decrypted accessory, large proportional shackle, and seamless pivot hinge without gap

--- src/XorIcon.tsx
+++ src/XorIcon.tsx
@@ -1,5 +1,6 @@
 /*
  * Encrypt Chat - Lock Icon Component
+ * Perfectly proportioned padlock with rooted shackle hinge animation
  */
 
 import { ChatBarButton, ChatBarButtonFactory } from "@api/ChatButtons";
@@ -27,19 +28,19 @@ export function XorIcon({ isLocked = true, width = 20, height = 20, className, s
             }}
             {...props}
         >
-            {/* Shackle: Connected directly into the body at y=10.5 */}
+            {/* Shackle: Left leg anchored deep into y=13 inside the body (top at y=10) */}
             <path
                 className={cl("shackle")}
                 fill="currentColor"
-                d="M7 10.5V6.5a5 5 0 0 1 10 0v4h-2.2V6.5a2.8 2.8 0 0 0-5.6 0v4H7z"
+                d="M6 13V6.5a6 6 0 0 1 12 0v4.5h-3.2V6.5a2.8 2.8 0 0 0-5.6 0V13H6z"
             />
-            {/* Lock Body with Keyhole */}
+            {/* Body: Solid rounded padlock body with keyhole */}
             <path
                 className={cl("body")}
                 fill="currentColor"
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M5 10a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-8a2 2 0 0 0-2-2H5zm7 3a1.5 1.5 0 0 1 1.5 1.5c0 .5-.26.96-.65 1.22l.65 2.28h-3l.65-2.28a1.5 1.5 0 0 1-.65-1.22A1.5 1.5 0 0 1 12 13z"
+                d="M4 10h16a2.5 2.5 0 0 1 2.5 2.5v7a2.5 2.5 0 0 1-2.5 2.5H4A2.5 2.5 0 0 1 1.5 19.5v-7A2.5 2.5 0 0 1 4 10zm8 3a1.5 1.5 0 0 0-1.5 1.5c0 .54.29.98.7 1.25L10.5 18h3l-.7-2.25c.41-.27.7-.71.7-1.25A1.5 1.5 0 0 0 12 13z"
             />
         </svg>
     );
